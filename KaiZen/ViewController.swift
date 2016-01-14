a12b04c24658bc8b94d54d03c03dcaf16fde3c2a
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-01-14 11:35:05 +0900

    Initial Commit

---
+++ KaiZen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KaiZen
+//
+//  Created by 堀江健太朗 on 2016/01/14.
+//  Copyright © 2016年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
