f07624d6abec55f6aa88ba27b66be592af76f5ce
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-02-10 23:07:09 +0900

    ChartViewModelファイル追加

---
+++ KaiZen/ChartViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ChartViewModel.swift
+//  KaiZen
+//
+//  Created by 堀江健太朗 on 2016/02/10.
+//  Copyright © 2016年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class ChartViewModel: NSObject/*, BEMSimpleLineGraphDelegate, BEMSimpleLineGraphDataSource*/ {
+    
+    override func awakeFromNib() {
+        
+    }
+    
+}
