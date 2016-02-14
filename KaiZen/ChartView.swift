8b36b1c64e8fe1419f121538c48cf72521eba0f9
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-02-15 01:47:35 +0900

    ひとまずのy軸のタイトル設定

--- KaiZen/ChartView.swift
+++ KaiZen/ChartView.swift
@@ -13,11 +13,36 @@ class ChartView: UIView {
     var graphView: BEMSimpleLineGraphView!
     
     override func awakeFromNib() {
-        graphView = BEMSimpleLineGraphView(frame: self.frame)
         
-        graphView.enableReferenceAxisFrame = true
+        setUP()
+        
+        let label = UILabel()
+        label.frame.size = CGSize(width: self.frame.width / 13, height: self.frame.height / 7 * 6)
+        label.frame.origin = CGPoint(x: 0, y: self.frame.height / 7)
+        label.backgroundColor = graphView.colorBottom
+        label.text = "改善具合"
+        label.textColor = UIColor.whiteColor()
+        label.numberOfLines = 4
+        self.addSubview(label)
+    }
+    
+    func setUP() {
+        setGraphView()
+        setTitleView()
+    }
+    
+    func setGraphView() {
+        graphView = BEMSimpleLineGraphView()
+        graphView.frame.size = CGSize(width: self.frame.width / 13 * 12, height: self.frame.height / 7 * 6)
+        graphView.frame.origin = CGPoint(x: self.frame.width / 13, y: self.frame.height / 7)
+        
+//        graphView.enableReferenceAxisFrame = true
         graphView.enableReferenceXAxisLines = true
         graphView.enableXAxisLabel = true
+        graphView.enableReferenceYAxisLines = true ////
+//        graphView.enableYAxisLabel = true ////
+        graphView.colorXaxisLabel = UIColor.whiteColor()
+        graphView.colorYaxisLabel = UIColor.whiteColor()
         graphView.enableBezierCurve = true
         graphView.autoScaleYAxis = true
         graphView.animationGraphEntranceTime = 3
@@ -25,9 +50,16 @@ class ChartView: UIView {
         self.addSubview(graphView)
     }
     
-//    func xAxisLabelWithText(text: String, index: Int) -> UILabel
-//    
-//    [4:42]
-//    let center = CGPointMake(positionOnXAxis, self.frame.size.height - lRect.size.height + 5)
+    func setTitleView() {
+        let label = UILabel()
+        label.frame.size = CGSize(width: self.frame.width, height: self.frame.height / 7 - 4)
+        label.frame.origin = CGPointZero
+        label.backgroundColor = graphView.colorBottom
+        label.text = "成長曲線"
+        label.textColor = UIColor.whiteColor()
+        label.textAlignment = NSTextAlignment.Center
+        
+        self.addSubview(label)
+    }
     
 }
