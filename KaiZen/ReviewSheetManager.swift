d07b64e9232f854e83b7080f92b73b94c7d4db3c
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-02-16 00:36:28 +0900

    デバイスへの保存＆取得　実装

--- KaiZen/ReviewSheetManager.swift
+++ KaiZen/ReviewSheetManager.swift
@@ -11,7 +11,7 @@ import UIKit
 class ReviewSheetManager {
     static let sharedInstance = ReviewSheetManager()
     var reviewSheetArray: [ReviewSheet] = []
-        var goal: [String: Int] = [:]
+//    var goal: [String: Int] = [:]
     
     func sumReviewPoint(reviewSheet: ReviewSheet) -> Int {
         var sumPoint = 0
@@ -20,5 +20,68 @@ class ReviewSheetManager {
         }
         
         return sumPoint
+    }
+    
+    //------------- save & fetch Devise --------------------
+    
+    class func saveForDevise() {
+        let saveReviewSheetArray = convertForSave()
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(saveReviewSheetArray, forKey: "reviewSheetArray")
+    }
+    
+    class func fetchFromDevise(callback: () -> Void ) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        guard let fetchReviewSheetArray = defaults.objectForKey("reviewSheetArray") as? Array<Array<Dictionary<String, AnyObject>>> else {
+            print("fetch error")
+            
+            callback()
+            return
+        }
+        
+        convertForFetch(fetchReviewSheetArray)
+        callback()
+    }
+    
+    //-------------------- convertData --------------
+    
+    class func convertForSave() -> Array<Array<Dictionary<String, AnyObject>>> {
+        var saveReviewSheetArray: Array<Array<Dictionary<String, AnyObject>>> = []
+        
+        for reviewSheet in sharedInstance.reviewSheetArray {
+            var reviewDicArray: Array<Dictionary<String, AnyObject>> = []
+            
+            for review in reviewSheet.reviewArray {
+                var dic: Dictionary<String, AnyObject> = [:]
+                
+                dic["reviewText"] = review.reviewText
+                dic["reviewPoint"] = review.reviewPoint
+                
+                reviewDicArray.append(dic)
+            }
+            
+            saveReviewSheetArray.append(reviewDicArray)
+        }
+        
+        return saveReviewSheetArray
+    }
+    
+    class func convertForFetch(fetchData: Array<Array<Dictionary<String, AnyObject>>>) {
+        for dicArray in fetchData {
+            let reviewSheet = ReviewSheet()
+            
+            for dic in dicArray {
+                let review = Review()
+                
+                review.reviewText = dic["reviewText"] as? String
+                review.reviewPoint = dic["reviewPoint"] as? Int
+                
+                reviewSheet.reviewArray.append(review)
+            }
+            
+            sharedInstance.reviewSheetArray.append(reviewSheet)
+        }
     }
 }
