1c692a530179c51797829fa671a03e5a24d2b5e7
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-02-18 09:34:30 +0900

    sideMenuファイル追加

---
+++ KaiZen/SideMenuView.swift
@@ -0,0 +1,21 @@
+//
+//  SideMenuView.swift
+//  KaiZen
+//
+//  Created by 堀江健太朗 on 2016/02/17.
+//  Copyright © 2016年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
