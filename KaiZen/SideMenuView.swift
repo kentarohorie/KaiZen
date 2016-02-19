d6933f15e29ff5f7ca9db826352c52564c54a07f
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-02-19 11:46:31 +0900

    sideMenu完成

--- KaiZen/SideMenuView.swift
+++ KaiZen/SideMenuView.swift
@@ -8,14 +8,65 @@
 
 import UIKit
 
+@objc protocol SideMenuViewDelegate {
+    optional func sideMenuDidRemoveSelf()
+}
+
 class SideMenuView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    var baseView: UIView!
+    weak var customDelegate: SideMenuViewDelegate?
+    var coverView: UIView = UIView()
+    
+    override func awakeFromNib() {
+        self.backgroundColor = UIColor.yellowColor()
+        selfSetting()
     }
-    */
-
+    
+    func selfSetting() {
+        self.hidden = true
+        coverView.backgroundColor = UIColor.clearColor()
+        self.backgroundColor = UIColor.clearColor()
+        
+        let tapGestureRecog = UITapGestureRecognizer(target: self, action: "removeSideMenu")
+        coverView.addGestureRecognizer(tapGestureRecog)
+    }
+    
+    func framingFromSuperView(baseView: UIView) {
+        self.baseView = baseView
+        
+        self.frame.size = CGSize(width: baseView.frame.width / 5 * 3, height: baseView.frame.height)
+        self.frame.origin = CGPoint(x: -(baseView.frame.width), y: 0)
+        
+        coverView.frame.size = CGSize(width: 0, height: baseView.frame.height)
+        coverView.frame.origin = CGPoint(x: self.frame.origin.x + self.frame.width, y: 0)
+                coverView.frame.width
+        baseView.addSubview(coverView)
+    }
+    
+    func appearSideMenu() {
+        self.hidden = false
+        
+        UIView.animateWithDuration(0.3) { () -> Void in
+            self.frame.origin.x = 0
+            
+            self.coverView.frame.origin.x = self.frame.width
+            self.coverView.frame.size = CGSize(width: self.baseView.frame.width - self.frame.width, height: self.baseView.frame.height)
+            self.coverView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        }
+    }
+    
+    func removeSideMenu() {
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.frame.origin.x = -(self.baseView.frame.width)
+            self.coverView.backgroundColor = UIColor.clearColor()
+            }) { (bool) -> Void in
+                self.removeFromSuperview()
+                guard let delegate = self.customDelegate else {
+                    return
+                }
+                delegate.sideMenuDidRemoveSelf!()
+        }
+    }
+    
 }
