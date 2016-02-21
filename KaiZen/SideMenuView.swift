fa1497ce28c9a81f85c8c1d05364f88330f7076e
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-02-21 22:56:56 +0900

    sideMenuのファイルのコードをclean

--- KaiZen/SideMenuView.swift
+++ KaiZen/SideMenuView.swift
@@ -9,42 +9,39 @@
 import UIKit
 
 @objc protocol SideMenuViewDelegate {
-    optional func sideMenuDidRemoveSelf()
-    optional func sideMenuDidPlus()
+    optional func sideMenuDidPlus(sheetName: String)
     optional func sideMenuDidMinus()
+    optional func sideMenuCellDidSelect(row: Int)
 }
 
-class SideMenuView: UIView, UITableViewDataSource, UITableViewDelegate {
+
+
+
+class SideMenuView: UIView, UITableViewDataSource, UITableViewDelegate, AddReviewViewDelegate {
 
     @IBOutlet weak var sideMenuTableView: UITableView!
     @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var minusButton: UIButton!
-    let reviewSheetManager = ReviewSheetManager.sharedInstance
-    
+    var addSheetView: AddReviewView?
     var baseView: UIView!
     weak var customDelegate: SideMenuViewDelegate?
     let coverView: UIView = UIView()
+    
+    let reviewSheetManager = ReviewSheetManager.sharedInstance
     
     override func awakeFromNib() {
         selfSetting()
     }
     
+    // ----- view setting -----------
+    
     func selfSetting() {
         self.hidden = true
         coverView.backgroundColor = UIColor.clearColor()
-        
-        let tapGestureRecog = UITapGestureRecognizer(target: self, action: "removeSideMenu")
-        coverView.addGestureRecognizer(tapGestureRecog)
-        let coverViewSwileLeftGestureRecog = UISwipeGestureRecognizer(target: self, action: "swipeLeft")
-        coverViewSwileLeftGestureRecog.direction = .Left
-        coverView.addGestureRecognizer(coverViewSwileLeftGestureRecog)
 
-        
-        let swipeLeftGestureRecog = UISwipeGestureRecognizer(target: self, action: "swipeLeft")
-        swipeLeftGestureRecog.direction = .Left
-        self.addGestureRecognizer(swipeLeftGestureRecog)
-        
         tableViewSetting()
+        buttonSetting()
+        setGesture()
     }
     
     func tableViewSetting() {
@@ -56,16 +53,31 @@ class SideMenuView: UIView, UITableViewDataSource, UITableViewDelegate {
         
     }
     
-    func buttonSetting() {
-        plusButton.layer.borderColor = UIColor.whiteColor().CGColor
-        plusButton.layer.borderWidth = 0.6
+    func setGesture() {
+        let tapGestureRecog = UITapGestureRecognizer(target: self, action: "removeSideMenu")
+        coverView.addGestureRecognizer(tapGestureRecog)
         
-        minusButton.layer.borderColor = UIColor.whiteColor().CGColor
-        minusButton.layer.borderWidth = 0.6
+        let coverViewSwileLeftGestureRecog = UISwipeGestureRecognizer(target: self, action: "removeSideMenu")
+        coverViewSwileLeftGestureRecog.direction = .Left
+        coverView.addGestureRecognizer(coverViewSwileLeftGestureRecog)
+        
+        
+        let swipeLeftGestureRecog = UISwipeGestureRecognizer(target: self, action: "removeSideMenu")
+        swipeLeftGestureRecog.direction = .Left
+        self.addGestureRecognizer(swipeLeftGestureRecog)
+
+    }
+    
+    func buttonSetting() {
+//        plusButton.layer.borderColor = UIColor.whiteColor().CGColor
+//        plusButton.layer.borderWidth = 0.3
+//        
+//        minusButton.layer.borderColor = UIColor.whiteColor().CGColor
+//        minusButton.layer.borderWidth = 0.3
         
     }
     
-    //------------ action -----------------
+    //------------ method -----------------
     
     func framingFromSuperView(baseView: UIView) {
         self.baseView = baseView
@@ -101,38 +113,25 @@ class SideMenuView: UIView, UITableViewDataSource, UITableViewDelegate {
                 UIApplication.sharedApplication().statusBarHidden = false
 
                 self.removeFromSuperview()
-                guard let delegate = self.customDelegate else {
-                    return
-                }
-//                delegate.sideMenuDidRemoveSelf!())
         }
     }
     
-    func swipeLeft() {
-        UIView.animateWithDuration(0.3, animations: { () -> Void in
-            self.frame.origin.x = -(self.baseView.frame.width)
-            self.coverView.frame.origin = CGPoint(x: self.frame.width - self.baseView.frame.width, y: 0)
-            self.coverView.backgroundColor = UIColor.clearColor()
-            }) { (bool) -> Void in
-                (self.superview?.superview?.superview as? UIScrollView)?.scrollEnabled = true
-                UIApplication.sharedApplication().statusBarHidden = false
-                
-                self.removeFromSuperview()
-                guard let delegate = self.customDelegate else {
-                    return
-                }
-                
-//                delegate.sideMenuDidRemoveSelf!()
-
-
+    func setAddView() {
+        addSheetView = UINib(nibName: "AddReviewView", bundle: nil).instantiateWithOwner(self, options: nil).first as? AddReviewView
+        addSheetView!.customDelegate = self
+        addSheetView?.center = CGPoint(x: self.superview!.center.x, y: -(addSheetView!.frame.height))
+        
+        UIView.animateWithDuration(0.8) { () -> Void in
+            self.addSheetView?.center.y = self.center.y
         }
+        
+        self.addSubview(addSheetView!)
     }
+    
+    // ----- receive event --------
     
     @IBAction func tapPlusButton(sender: UIButton) {
-        guard let delegate = customDelegate else {
-            return
-        }
-        delegate.sideMenuDidPlus!()
+        setAddView()
     }
     
     @IBAction func tapMinusButton(sender: UIButton) {
@@ -141,6 +140,16 @@ class SideMenuView: UIView, UITableViewDataSource, UITableViewDelegate {
         }
         delegate.sideMenuDidMinus!()
     }
+    
+    func tapAddOfAddView(text: String) {
+        guard let delegate = customDelegate else {
+            return
+        }
+        delegate.sideMenuDidPlus!(text)
+        sideMenuTableView.reloadData() //callbackで
+    }
+    
+    //--------- likely modelview... ------------->>>
     
     //----------- tableView datasource --------------
     
@@ -158,6 +167,8 @@ class SideMenuView: UIView, UITableViewDataSource, UITableViewDelegate {
         cell.selectionStyle = .None
         cell.isSelectView.backgroundColor = UIColor.clearColor()
         
+        cell.titleLabel.text = reviewSheetManager.reviewSheetArray[indexPath.row].title
+        
         return cell
     }
     
@@ -168,8 +179,6 @@ class SideMenuView: UIView, UITableViewDataSource, UITableViewDelegate {
         UIView.animateWithDuration(0.2) { () -> Void in
             didSelectCell.isSelectView.backgroundColor = UIColor.clearColor()
         }
-        
-
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
@@ -177,6 +186,11 @@ class SideMenuView: UIView, UITableViewDataSource, UITableViewDelegate {
         UIView.animateWithDuration(0.2) { () -> Void in
             selectedCell.isSelectView.backgroundColor = UIColor(red: 2/255, green: 168/255, blue: 243/255, alpha: 1)
         }
+        guard let delegate = customDelegate else {
+            return
+        }
+        delegate.sideMenuCellDidSelect!(indexPath.row)
+        self.removeSideMenu()
     }
     
 }
