f26f344ac7330f06ec2fc41649a1228d808cf0ca
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-03-12 20:07:03 +0900

    サイドメニューのsheet追加画面について
    ジェスチャーの諸問題をひとまずキーボードを自動的に出すようにして解決

--- KaiZen/AddReviewView.swift
+++ KaiZen/AddReviewView.swift
@@ -68,6 +68,7 @@ class AddReviewView: UIView, UITextFieldDelegate {
     
     @IBAction func tapClose(sender: UIButton) {
         closeAnimation()
+        textField.resignFirstResponder()
     }
     
     
