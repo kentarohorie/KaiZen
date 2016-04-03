25e389e46d6eef0c914037dd4db69ed1b5b7a08a
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-04-03 16:06:11 +0900

    せつめいview実装のため画像やswiftファイル追加

---
+++ KaiZen/walkThroughView.swift
@@ -0,0 +1,21 @@
+//
+//  walkThroughView.swift
+//  KaiZen
+//
+//  Created by 堀江健太朗 on 2016/04/03.
+//  Copyright © 2016年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class walkThroughView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
